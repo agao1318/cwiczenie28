d2b7c2ec2f753ac5df9a68b54a0d51dbc06d453b
agao1318 <agao1318@gmail.com>
2022-05-21 08:16:22 +0200

    poprawka

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@ let number = 12;
 if (number > 10) { console.log('Podana liczba jest większa od 10')
 }
 
-else if (number < 10) {console.log('podana liczba nie jest większa od 10')
+else {console.log('podana liczba nie jest większa od 10')
 };
